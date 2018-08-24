9ac0a96a5eb0e157734a22c3eba38d8c61926aeb
RicardoNicoleti <31298466+RicardoNicoleti@users.noreply.github.com>
2018-08-24 20:19:00 -0300

    Alterações 24/08/2018

--- src/main/java/br/edu/unifacear/catalogoautomotivo/entity/PecaModelo.java
+++ src/main/java/br/edu/unifacear/catalogoautomotivo/entity/PecaModelo.java
@@ -1,8 +1,23 @@
 package br.edu.unifacear.catalogoautomotivo.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 public class PecaModelo {
+	 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
 	private Peca peca;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
 	private Modelo modelo;
 	
 	public Long getId() {
